5c1ada7476f40972ec3849803514a871613e4e40
Thalles <thallesemail@mail.com>
2026-02-07 20:47:29 -0300

    Iniciando programa para calculo de hipotenusa, ja criado as entradas e feito a soma dos catetos ao quadrado

---
+++ hypotenuse.py
@@ -0,0 +1,9 @@
+import math
+
+print("Bem vindo ao algoritimo de cálculo de hipotenusa \nVamos começar?")
+
+number_a = float(input("Qual é o primeiro número do cateto: "))
+number_b = float(input("Qual é o segundo número do cateto: "))
+
+exponent_number = math.pow(number_a,2) + math.pow(number_b,2)
+
