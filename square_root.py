2a0a36b933222146643606074d2b4829d0742266
Thalles <thallesemail@mail.com>
2026-02-07 14:19:36 -0300

    Função de calculo de raiz quadrada e quadrado do número digitado adicionado

--- square_root.py
+++ square_root.py
@@ -1,4 +1,10 @@
 import math
 
 print("Vamos mostrar a raíz quadrada do seu número, mas primeiro preciso que você digite ele.")
-number_usu = input("Qual número que você quer escolher: ")
+number_usu = int(input("Qual número que você quer escolher: "))
+
+square_number = math.isqrt(number_usu)
+exponent_number = math.pow(number_usu,2)
+
+print(square_number)
+print(exponent_number)
