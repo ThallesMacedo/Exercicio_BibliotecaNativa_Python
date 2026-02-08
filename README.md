d6bf08cccf04c6f0cecec4c364a101cb2b5ac470
Thalles <thallesemail@mail.com>
2026-02-07 21:25:10 -0300

    Realizado o algoritmo conforme solicitado no exercício, feito a fórmula d o calculo e entregue o usuário a resposta esperada.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Exercício 1 (Square_root.py): Peça ao usuário um número e mostre a raiz quadrada do número, o quadrado do
 número e a raiz quadrada arredondada para duas casas decimais.
+
+Exercício 2 (Hypotenuse.py): Peça dois números ao usuário e calcule a hipotenusa de um triângulo retângulo.
--- hypotenuse.py
+++ hypotenuse.py
@@ -1,9 +1,15 @@
+#Importando a Biblioteca de funções matematicas
 import math
 
+#Recebendo os numeros referente aos catetos
 print("Bem vindo ao algoritimo de cálculo de hipotenusa \nVamos começar?")
 
-number_a = float(input("Qual é o primeiro número do cateto: "))
-number_b = float(input("Qual é o segundo número do cateto: "))
+number_a = float(input("Qual é o primeiro número do cateto (cm): "))
+number_b = float(input("Qual é o segundo número do cateto: (cm)"))
 
+#Fazendo o calculo por parte, primeiro realizo a soma dos catetos ao quadrado e o resultado eu retiro a raíz quadrada
 exponent_number = math.pow(number_a,2) + math.pow(number_b,2)
+hypotenuse = math.sqrt(exponent_number)
 
+#Devolvo para o usuário o resultado da hipotenusa
+print(f"A hipotenusa calculada com os catetos inseridos é de: {hypotenuse:.2f} cm")
