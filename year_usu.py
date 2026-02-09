92a0c164214cfce9c03230ad5d8243535a9617dd
Thalles <thallesemail@mail.com>
2026-02-09 19:54:31 -0300

    Finalizado a proposta do exercicio e o algoritimo está apresentando a idade aproximada do usuário

--- year_usu.py
+++ year_usu.py
@@ -1,1 +1,13 @@
+#Importando biblioteca para uso de datas
 import datetime
+
+#Recebo o dado do usuário em qual ano ele nasceu e puxo uma função para saber a data atual transformando o ano atual em inteiro
+year_usu = int(input("Qual ano que você nasceu? "))
+today_and_hour = datetime.datetime.now()
+year_today = int(today_and_hour.strftime("%Y"))
+
+#Faço a fórmula do ano atual menos o ano inserido pelo usuário
+years = year_today - year_usu
+
+#Apresentando o resultado
+print(f"Sua idade aproximada é de: {years} anos")
